a8a2ebf18784821f6fb8b852b060bac76d0c6c9b
umairalipathan1980 <112640249+umairalipathan1980@users.noreply.github.com>
2025-01-09 22:14:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ with st.sidebar:
         st.session_state.selected_model = "llama-3.3-70b-versatile"
 
     model_list = [
+        "llama-3.1-8b-instant",
         "llama-3.3-70b-versatile",
-        "llama3-70b-8192",  
-        "llama-3.1-8b-instant", 
+        "llama3-70b-8192",   
         "llama3-8b-8192", 
         "mixtral-8x7b-32768", 
         "gemma2-9b-it",
