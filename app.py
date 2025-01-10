9f7bdff993c7388a0545962f4b88525cc35e02bc
umairalipathan1980 <112640249+umairalipathan1980@users.noreply.github.com>
2025-01-10 17:04:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 from agentic_rag import initialize_app
 import sys
