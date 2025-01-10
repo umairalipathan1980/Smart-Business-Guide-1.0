13dcbfd56e13a8759363cb1ece5b61ecff413bf9
umairalipathan1980 <112640249+umairalipathan1980@users.noreply.github.com>
2025-01-10 18:32:05 +0200

    Update agentic_rag.py

--- agentic_rag.py
+++ agentic_rag.py
@@ -228,23 +228,12 @@ def initialize_llm(model_name, answer_style):
     return st.session_state.llm
 
 
-# def initialize_embedding_model(selected_embedding_model):
-#     if "embed_model" not in st.session_state or st.session_state.embed_model.model != selected_embedding_model:
-#         if "text-" in selected_embedding_model:
-#             st.session_state.embed_model = OpenAIEmbeddings(model=selected_embedding_model)
-#         else:
-#             st.session_state.embed_model = HuggingFaceEmbeddings(model_name=selected_embedding_model)
-    
-#     return st.session_state.embed_model
-
 def initialize_embedding_model(selected_embedding_model):
-    if "embed_model" not in st.session_state or not hasattr(st.session_state.embed_model, 'model_identifier') or st.session_state.embed_model.model_identifier != selected_embedding_model:
+    if "embed_model" not in st.session_state or st.session_state.embed_model.model != selected_embedding_model:
         if "text-" in selected_embedding_model:
             st.session_state.embed_model = OpenAIEmbeddings(model=selected_embedding_model)
-            st.session_state.embed_model.model_identifier = selected_embedding_model
         else:
             st.session_state.embed_model = HuggingFaceEmbeddings(model_name=selected_embedding_model)
-            st.session_state.embed_model.model_identifier = selected_embedding_model
     
     return st.session_state.embed_model
 
