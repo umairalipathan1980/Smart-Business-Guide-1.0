7e49d6e8749462eb1bd37dccd9ce6308526dd142
umairalipathan1980 <112640249+umairalipathan1980@users.noreply.github.com>
2025-01-13 22:14:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ if "messages" not in st.session_state:
 
 # Sidebar layout
 with st.sidebar:
-    try:
+    # try:
     #     st.image("LOGO_UPBEAT.jpg", width=150, use_container_width=True)
     # except Exception as e:
     #     st.warning("Unable to load image. Continuing without it.")
