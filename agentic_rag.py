3e962efccce998c8cd972fc68daab9cfc173c693
umairalipathan1980 <112640249+umairalipathan1980@users.noreply.github.com>
2025-02-10 17:27:28 +0200

    Update agentic_rag.py

--- agentic_rag.py
+++ agentic_rag.py
@@ -319,7 +319,7 @@ rag_prompt = PromptTemplate(
 
                 5. **Citation Rules**:
                 - Citation information may be present in the context in the form of [document name, page number] or URLs. It is very important to cite references if you find them in the context.
-                - For responses based on the context documents containing 'page_content' field of 'Smart guide results:', cite the document name and page number with each piece of information in the format (bold fonts): [document_name, page xx].
+                - For responses based on the context documents containing 'page_content' field of 'Smart guide results:', cite the document name and page number with each piece of information in the format: [document_name, page xx].
                 - For the answer compiled from the context from multiple documents, use the format: document_name 1 [page xx, yy, zz, ...], document_name 2 [page xx, yy, zz, ...].
                 - For responses based on the documents containing the 'page_content' field of 'Internet search results:', include all the URLs in hyperlink form returned by the websearch, each on a new line. **very important**: The URLs should be labelled with the website name. 
                 - Do not invent any citation or URL. Only use the citation or URL in the context. 
